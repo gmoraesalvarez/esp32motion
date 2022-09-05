6a517c1038c2c871a0c5ba8dbb7f506077478a32
Guilherme de Moraes Alvarez <moraes.alvarez@gmail.com>
2022-09-05 16:55:50 -0300

    Create play.php

---
+++ play.php
@@ -0,0 +1,5 @@
+<html style="background:#323232;text-align:center;">
+<video style="height:100%;background:#323232;display:inline-block;" src="<?php echo $_GET['url']; ?>" controls>
+   Your browser does not support the <code>video</code> element.
+</video>
+</html>
